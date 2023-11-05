cc7d9ff3a7e705abad9ec902776c0a5903faa9cf
jayIsThere <149234876+jayIsThere@users.noreply.github.com>
2023-11-05 18:35:06 +0000

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Jaehan Kim",
+  title: "Computer Science Student",
+  email: "jaehan.kim@studmail.w-hs.de",
+  gitHub: "JayIsThere",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "jaehanshowraincloud",
 };
 
 const primaryColor = "#4E567E";
